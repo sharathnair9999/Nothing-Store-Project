2e21a1b805d72c55f10df415a856e2975d9d3628
Sharath <sharathnair9999@gmail.com>
2022-03-20 00:31:53 +0530

    Feat : Added responsive navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="top-navbar">
+      <input type="checkbox" id="check" />
+      <nav className="header">
+        <div className="icon flex-and-center">
+            <Link to="/" className="flex-and-center">
+              <img
+                src="https://res.cloudinary.com/sharath-media-library/image/upload/v1647446845/nothing-store-project/Nothing_1_zikesp.png"
+                alt="logo"
+              />
+              <span>Store</span>
+            </Link>
+        </div>
+        <div className="search_box">
+          <input type="search" placeholder="Search Here..." />
+          <span className="fa-solid fa-magnifying-glass"></span>
+        </div>
+        <ol className="nav-actions flex">
+          <li>
+            <Link to="/user/cart">
+              <div className="badge-container mx-1">
+                <i className="fas fa-cart-shopping fa-lg"></i>
+                <span className="icon-badge green-badge flex-and-center">
+                  10
+                </span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/user/wishlist">
+              <div className="badge-container mx-1">
+                <i className="fa-solid fa-heart fa-lg"></i>
+                <span className="icon-badge green-badge flex-and-center">
+                  2
+                </span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/user">
+              <i className="fa-solid fa-user fa-lg"></i>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/login" className="btn btn-outline-primary">
+              <b>Login</b>
+            </Link>
+          </li>
+        </ol>
+        <label htmlFor="check" className="bar">
+          <span className="fa-solid fa-bars" id="bars"></span>
+          <span className="fa-solid fa-times" id="times"></span>
+        </label>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
