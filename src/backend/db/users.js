44ae6c0eecbdd3bdbfda8f54653eaeadcae86d85
Sharath Nair <61535475+sharathnair9999@users.noreply.github.com>
2022-06-06 13:07:57 +0530

    updated test user info

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Chandler",
+    lastName: "Bing",
+    email: "chandlerbing@friends.com",
+    password: "chandlerbing",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
